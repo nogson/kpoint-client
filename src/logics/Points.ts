2811888430e87acc8b0b12e9d0242caa81de0b4c
佐藤文彦 <fsato@ciaoinc.jp>
2021-10-29 22:03:09 +0900

    no message

--- src/logics/Points.ts
+++ src/logics/Points.ts
@@ -4,11 +4,25 @@ import {Point} from '@/logics/Point'
 const windowW = window.innerWidth
 
 export class Points {
-    public container: PIXI.Container = new PIXI.Container()
+    public container = new PIXI.Container()
+    private pointSize = 100
 
-    constructor(maxPoints = 100) {
+    constructor(maxPoints = 30) {
         const positions = this.getPosition(maxPoints)
 
+        // line
+        for (let i = 0, j = positions.length - 1; i < j; i++) {
+            const line = new PIXI.Graphics()
+            const margin: number = this.pointSize / 2
+            const startX: number = positions[i].x + margin
+            const startY: number = positions[i].y + margin
+            const endX: number = positions[i + 1].x + margin
+            const endY: number = positions[i + 1].y + margin
+            line.lineStyle(22, 0xF9C200).moveTo(startX, startY).lineTo(endX, endY)
+            this.container.addChild(line)
+        }
+
+        // point
         for (let i = 0, j = positions.length; i < j; i++) {
             const props = {
                 text: String(i + 1),
@@ -23,11 +37,11 @@ export class Points {
 
     private getPosition = (maxPoints: number): { x: number, y: number }[] => {
         const margin = 25
-        const pointSize = 100 + margin
+        const pointSize = this.pointSize + margin
         const positions: { x: number, y: number }[] = []
         const maxLength = Math.floor((windowW - pointSize) / pointSize)
         let direction = 1
-        let x = (windowW - ((maxLength + 1) * pointSize - margin)) /2
+        let x = (windowW - ((maxLength + 1) * pointSize - margin)) / 2
         let y = margin
         for (let i = 0; i < maxPoints; i++) {
 
