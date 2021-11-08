fbc3c9128c129e6aaf3007e0f273939a7ec0fcd7
佐藤文彦 <fsato@ciaoinc.jp>
2021-11-08 22:33:54 +0900

    no message

---
+++ src/firebase/firestore.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import {getAnalytics} from "firebase/analytics"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+//const settings = { timestampsInSnapshots: true }
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIERBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIERBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIERBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIERBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIERBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIERBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIERBASE_MEASUREMENT_ID
+}
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+const db = firebase.firestore()
+export default db
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,31 +3,42 @@
 </template>
 
 <script lang="ts">
-    import {defineComponent, reactive, onMounted, ref} from 'vue'
-    import {Stage} from "@/logics/Stage"
+import {defineComponent, reactive, onMounted, ref} from 'vue'
+import {Stage} from "@/logics/Stage"
+import db from "@/firebase/firestore.ts"
 
-    interface State {
-        stage: Stage | null;
-    }
+interface State {
+  stage: Stage | null;
+}
 
-    export default defineComponent({
-        components: {},
-        setup() {
-            let canvasRef = ref<HTMLCanvasElement | null>(null)
-            let {stage} = reactive<State>({
-                stage: null
-            })
-            onMounted(() => {
-                if (canvasRef.value) {
-                    stage = new Stage(canvasRef.value)
-                }
-            })
-
-            return {
-                canvasRef
-            }
-        }
+export default defineComponent({
+  components: {},
+  setup() {
+    let canvasRef = ref<HTMLCanvasElement | null>(null)
+    let {stage} = reactive<State>({
+      stage: null
     })
+    onMounted(async() => {
+      // var docRef = db.collection("presents")
+      // await docRef.add(
+      //     { name: "任天堂Switch",point:1000}
+      // )
+      const querySnapshot =  await db.collection("presents").get()
+
+      querySnapshot.forEach(doc => {
+        console.log(doc.data())
+      })
+
+      if (canvasRef.value) {
+        stage = new Stage(canvasRef.value)
+      }
+    })
+
+    return {
+      canvasRef
+    }
+  }
+})
 
 </script>
 
