ba72f68481104ad7f88586205aa1c73a05130589
佐藤文彦 <fsato@ciaoinc.jp>
2021-12-09 21:55:36 +0900

    no message

--- src/views/AddPoint.vue
+++ src/views/AddPoint.vue
@@ -12,20 +12,22 @@
     <!-- モーダル -->
     <teleport to="body">
       <transition name="fade">
-        <div v-if="state.isModalOpen" class='dialog'>
-          <rotate-circle>
-            <div>
-              <p>
-                がんばったね！
-              </p>
-              <p class='get-point'>
-                1ポイントゲット！
-              </p>
-              <div class="current-point">
-                <user-point/>
-                <span>ポイント</span></div>
-            </div>
-          </rotate-circle>
+        <div class="dialog-wrapper" v-if="state.isModalOpen">
+          <div class='dialog'>
+            <rotate-circle>
+              <div>
+                <p>
+                  がんばったね！
+                </p>
+                <p class='get-point'>
+                  1ポイントゲット！
+                </p>
+                <div class="current-point">
+                  <user-point/>
+                  <span>ポイント</span></div>
+              </div>
+            </rotate-circle>
+          </div>
         </div>
       </transition>
     </teleport>
@@ -34,161 +36,171 @@
 
 
 <script lang='ts'>
-import {computed, defineComponent, onBeforeMount, reactive} from 'vue'
-import {useStore} from 'vuex'
-import BackButton from '@/components/BackButton.vue'
-import UserPoint from '@/components/UserPoint.vue'
-import {gsap} from 'gsap'
-import RotateCircle from "@/components/RotateCircle.vue"
+    import {computed, defineComponent, onBeforeMount, reactive} from 'vue'
+    import {useStore} from 'vuex'
+    import BackButton from '@/components/BackButton.vue'
+    import UserPoint from '@/components/UserPoint.vue'
+    import {gsap} from 'gsap'
+    import RotateCircle from "@/components/RotateCircle.vue"
 
-interface State {
-  isModalOpen: boolean
-}
+    interface State {
+        isModalOpen: boolean
+    }
 
-export default defineComponent({
-  components: {RotateCircle, BackButton, UserPoint},
-  setup() {
-    const store = useStore()
-    const points = computed(() => store.getters.points)
-    const state = reactive<State>({
-      isModalOpen: false
+    export default defineComponent({
+        components: {RotateCircle, BackButton, UserPoint},
+        setup() {
+            const store = useStore()
+            const points = computed(() => store.getters.points)
+            const state = reactive<State>({
+                isModalOpen: false
+            })
+
+            const addPoint = async (point: number) => {
+                await store.dispatch('setPoint', point)
+                state.isModalOpen = true
+                setTimeout(() => {
+                    state.isModalOpen = false
+                }, 3000)
+            }
+
+            return {
+                points,
+                addPoint,
+                state
+            }
+        }
     })
-
-    const addPoint = async (point: number) => {
-      await store.dispatch('setPoint', point)
-      state.isModalOpen = true
-      setTimeout(() => {
-        state.isModalOpen = false
-      }, 3000)
-    }
-
-    return {
-      points,
-      addPoint,
-      state
-    }
-  }
-})
 </script>
 
 <style scoped lang='scss'>
-#add-points {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: calc(100vh - 64px);
-}
+  #add-points {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 64px);
+  }
 
-.points-wrap {
-  position: relative;
-  left: 0;
-  width: 332px;
-  background: #00C2FF;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 100px 100px 20px 20px;
-  padding: 90px 24px 24px;
+  .points-wrap {
+    position: relative;
+    left: 0;
+    width: 332px;
+    background: #00C2FF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 100px 100px 20px 20px;
+    padding: 90px 24px 24px;
 
-  h1 {
-    position: absolute;
-    left: (332px - 395px) /2;
-    top: -20px;
-    //transform: translate(50%,0);
-    width: 395px;
-    height: 91px;
-    font-size: 32px;
-    background: url('../assets/images/add_point_title.png') no-repeat center;
-    background-size: contain;
-    padding-top: 7px;
+    h1 {
+      position: absolute;
+      left: (332px - 395px) /2;
+      top: -20px;
+      //transform: translate(50%,0);
+      width: 395px;
+      height: 91px;
+      font-size: 32px;
+      background: url('../assets/images/add_point_title.png') no-repeat center;
+      background-size: contain;
+      padding-top: 7px;
+      font-weight: $font-black;
+      text-shadow: 0 0 5px darken($color-secondary, 20%);
+      text-align: center;
+      color: #FFF;
+    }
+  }
+
+  .point {
+    cursor: pointer;
+    font-size: 16px;
     font-weight: $font-black;
-    text-shadow: 0 0 5px darken($color-secondary, 20%);
-    text-align: center;
-    color: #FFF;
-  }
-}
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 40px;
+    color: $color-black;
+    margin-bottom: 24px;
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    text-align: left;
 
-.point {
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: $font-black;
-  background: #FFFFFF;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 40px;
-  color: $color-black;
-  margin-bottom: 24px;
-  padding: 24px;
-  display: flex;
-  align-items: center;
-  text-align: left;
-
-  .value {
-    display: block;
-    height: 70px;
-    width: 70px;
-    line-height: 70px;
-    font-size: 24px;
-    color: $color-primary;
-    background: url('../assets/images/icon_point.png');
-    margin-right: 18px;
-    flex-shrink: 0;
-    text-align: center;
-  }
-}
-
-.back-button {
-  position: absolute;
-  left: 24px;
-  top: 24px;
-}
-
-
-.dialog {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 479px;
-  height: 468px;
-  font-size: 16px;
-
-  .get-point {
-    font-size: 24px;
-    font-weight: $font-black;
-    color: $color-primary;
-  }
-
-  .current-point {
-    display: inline-block;
-
-    > span:last-child {
+    .value {
       display: block;
-      font-size: 14px;
+      height: 70px;
+      width: 70px;
+      line-height: 70px;
+      font-size: 24px;
+      color: $color-primary;
+      background: url('../assets/images/icon_point.png');
+      margin-right: 18px;
+      flex-shrink: 0;
       text-align: center;
     }
   }
-}
 
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-  top: 75%;
-}
-
-@keyframes rotate {
-  0% {
-    transform: rotateZ(0);
+  .back-button {
+    position: absolute;
+    left: 24px;
+    top: 24px;
   }
-  100% {
-    transform: rotateZ(360deg);
+
+  .dialog-wrapper {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    right: 100%;
+    top: 0;
+    bottom: 100%;
+    background: rgba(#FFF,0.8);
   }
-}
+
+  .dialog {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 479px;
+    height: 468px;
+    font-size: 16px;
+
+    .get-point {
+      font-size: 24px;
+      font-weight: $font-black;
+      color: $color-primary;
+    }
+
+    .current-point {
+      display: inline-block;
+
+      > span:last-child {
+        display: block;
+        font-size: 14px;
+        text-align: center;
+      }
+    }
+  }
+
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    top: 75%;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotateZ(0);
+    }
+    100% {
+      transform: rotateZ(360deg);
+    }
+  }
 </style>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -19,21 +19,20 @@
     <!-- モーダル -->
     <teleport to="body">
       <transition name="fade">
-        <div v-if="state.isModalOpen" class='dialog'>
-          <rotate-circle>
-            <div>
-              <p>
-                おめでとう！
-              </p>
-              <p><img :src="state.selectedPresent.thumbnail"></p>
-              <p class='get-point'>
-                {{ state.selectedPresent.name }}ゲット！
-              </p>
-              <div class="current-point">
-                <user-point/>
-                <span>ポイント</span></div>
-            </div>
-          </rotate-circle>
+        <div v-if="state.isModalOpen" class="dialog-wrapper">
+          <div class='dialog'>
+            <rotate-circle>
+              <div>
+                <p><img :src="state.selectedPresent.thumbnail"></p>
+                <p class='get-point'>
+                  {{ state.selectedPresent.name }}ゲット！
+                </p>
+                <div class="current-point">
+                  <user-point/>
+                  <span>ポイント</span></div>
+              </div>
+            </rotate-circle>
+          </div>
         </div>
       </transition>
     </teleport>
@@ -42,217 +41,231 @@
 
 
 <script lang='ts'>
-import {computed, defineComponent, onBeforeMount, reactive} from "vue"
-import {useStore} from "vuex"
-import BackButton from '@/components/BackButton.vue'
-import UserPoint from '@/components/UserPoint.vue'
-import EmoticonCryOutline from 'vue-material-design-icons/EmoticonCryOutline.vue'
-import RotateCircle from "@/components/RotateCircle.vue"
+    import {computed, defineComponent, onBeforeMount, reactive} from "vue"
+    import {useStore} from "vuex"
+    import BackButton from '@/components/BackButton.vue'
+    import UserPoint from '@/components/UserPoint.vue'
+    import EmoticonCryOutline from 'vue-material-design-icons/EmoticonCryOutline.vue'
+    import RotateCircle from "@/components/RotateCircle.vue"
 
-interface Present {
-  name: string
-  point: number
-  thumbnail: string
-}
+    interface Present {
+        name: string
+        point: number
+        thumbnail: string
+    }
 
-interface State {
-  isModalOpen: boolean,
-  selectedPresent: Present | null,
-  isInsufficientPoint: boolean
-}
+    interface State {
+        isModalOpen: boolean,
+        selectedPresent: Present | null,
+        isInsufficientPoint: boolean
+    }
 
-export default defineComponent({
-  components: {
-    RotateCircle,
-    BackButton,
-    UserPoint,
-    EmoticonCryOutline
-  },
-  setup() {
-    const state = reactive<State>({
-      isModalOpen: false,
-      selectedPresent: null,
-      isInsufficientPoint: false
+    export default defineComponent({
+        components: {
+            RotateCircle,
+            BackButton,
+            UserPoint,
+            EmoticonCryOutline
+        },
+        setup() {
+            const state = reactive<State>({
+                isModalOpen: false,
+                selectedPresent: null,
+                isInsufficientPoint: false
+            })
+            const store = useStore()
+            const presents = computed(() => store.getters.presents)
+
+            const getPresent = async (present: Present) => {
+
+                if (0 > store.getters.userSate.point - present.point) {
+                    state.isInsufficientPoint = true
+                    return
+                }
+
+                await store.dispatch('setPoint', -present.point)
+                state.selectedPresent = present
+                state.isModalOpen = true
+                state.isInsufficientPoint = false
+                setTimeout(() => {
+                    state.isModalOpen = false
+                    state.selectedPresent = null
+                }, 3000)
+            }
+
+            return {
+                presents,
+                getPresent,
+                state
+            }
+        }
     })
-    const store = useStore()
-    const presents = computed(() => store.getters.presents)
-
-    const getPresent = async (present: Present) => {
-
-      if (0 > store.getters.userSate.point - present.point) {
-        state.isInsufficientPoint = true
-        return
-      }
-
-      await store.dispatch('setPoint', -present.point)
-      state.selectedPresent = present
-      state.isModalOpen = true
-      state.isInsufficientPoint = false
-      setTimeout(() => {
-        state.isModalOpen = false
-        state.selectedPresent = null
-      }, 50000000)
-    }
-
-    return {
-      presents,
-      getPresent,
-      state
-    }
-  }
-})
 </script>
 
 <style scoped lang='scss'>
-#shop {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-
-.present-wrap {
-  position: relative;
-  left: 0;
-  width: 332px;
-  background: $color-secondary;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 100px 100px 20px 20px;
-  padding: 90px 24px 24px;
-
-  h1 {
-    text-align: center;
-    position: absolute;
-    left: (332px - 380px) /2;
-    top: -20px;
-    //transform: translate(50%,0);
-    width: 380px;
-    height: 91px;
-    font-size: 32px;
-    background: url("../assets/images/shop_title.png") no-repeat center;
-    background-size: contain;
-    padding-top: 7px;
-    font-weight: $font-black;
-    text-shadow: 0 0 5px darken($color-pink, 20%);
-    color: #FFF;
-  }
-}
-
-.present {
-  font-weight: $font-black;
-  background: #FFFFFF;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 40px;
-  color: $color-black;
-  margin-bottom: 24px;
-  padding: 24px;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-
-  dt {
-    width: 80px;
-  }
-
-  dd {
-    flex: 1;
-    margin-left: 16px;
-    font-size: 14px;
-    text-align: left;
-
-    p {
-      margin-bottom: 8px;
-      font-weight: $font-bold;
-    }
-  }
-
-  .value {
-    display: inline-flex;
-    font-size: 24px;
-    color: $color-black;
+  #shop {
     display: flex;
-
-    i {
-      width: 32px;
-      height: 32px;
-      margin: 0 8px 0 auto;
-      background: url("../assets/images/k_point_s.png") no-repeat center left;
-    }
-  }
-}
-
-.back-button {
-  position: absolute;
-  left: 24px;
-  top: 24px;
-}
-
-.insufficient-point {
-  font-size: 24px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 18px;
-  font-weight: bold;
-
-  span:first-child {
-    display: block;
-    margin: -6px 8px 0 0;
-  }
-}
-
-.dialog {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 479px;
-  height: 468px;
-  font-size: 16px;
-  img {
-    text-align: center;
-    margin: 0 auto;
-
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
   }
 
-  .get-point {
-    font-size: 24px;
-    font-weight: $font-black;
-    color: $color-primary;
-  }
+  .present-wrap {
+    position: relative;
+    left: 0;
+    width: 332px;
+    background: $color-secondary;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 100px 100px 20px 20px;
+    padding: 90px 24px 24px;
 
-  .current-point {
-    .user-point{
+    h1 {
+      text-align: center;
+      position: absolute;
+      left: (332px - 380px) /2;
+      top: -20px;
+      //transform: translate(50%,0);
+      width: 380px;
+      height: 91px;
       font-size: 32px;
+      background: url("../assets/images/shop_title.png") no-repeat center;
+      background-size: contain;
+      padding-top: 7px;
+      font-weight: $font-black;
+      text-shadow: 0 0 5px darken($color-pink, 20%);
+      color: #FFF;
     }
-    > span:last-child {
+  }
+
+  .present {
+    font-weight: $font-black;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 40px;
+    color: $color-black;
+    margin-bottom: 24px;
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    dt {
+      width: 80px;
+    }
+
+    dd {
+      flex: 1;
+      margin-left: 16px;
       font-size: 14px;
+      text-align: left;
+
+      p {
+        margin-bottom: 8px;
+        font-weight: $font-bold;
+      }
+    }
+
+    .value {
+      display: inline-flex;
+      font-size: 24px;
+      color: $color-black;
+      display: flex;
+
+      i {
+        width: 32px;
+        height: 32px;
+        margin: 0 8px 0 auto;
+        background: url("../assets/images/k_point_s.png") no-repeat center left;
+      }
     }
   }
-}
 
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-  top: 75%;
-}
-
-@keyframes rotate {
-  0% {
-    transform: rotateZ(0);
+  .back-button {
+    position: absolute;
+    left: 24px;
+    top: 24px;
   }
-  100% {
-    transform: rotateZ(360deg);
+
+  .insufficient-point {
+    font-size: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 18px;
+    font-weight: bold;
+
+    span:first-child {
+      display: block;
+      margin: -6px 8px 0 0;
+    }
   }
-}
+
+  .dialog-wrapper {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    right: 100%;
+    top: 0;
+    bottom: 100%;
+    background: rgba(#FFF,0.8);
+  }
+
+  .dialog {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 479px;
+    height: 468px;
+    font-size: 16px;
+
+    img {
+      text-align: center;
+      margin: 0 auto;
+      width: 75px;
+
+    }
+
+    .get-point {
+      font-size: 24px;
+      font-weight: $font-black;
+      color: $color-primary;
+    }
+
+    .current-point {
+      .user-point {
+        font-size: 32px;
+      }
+
+      > span:last-child {
+        font-size: 14px;
+      }
+    }
+  }
+
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    top: 75%;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotateZ(0);
+    }
+    100% {
+      transform: rotateZ(360deg);
+    }
+  }
 </style>
